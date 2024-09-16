74dc92148f02eab033384dd8c60c9ee130e242fa
Amaury Fierens <49645350+amauryfierens@users.noreply.github.com>
2024-09-16 11:27:47 +0200

    Update flue.py

--- tasks/flue.py
+++ tasks/flue.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# (c) Amaury Fierens, UCLouvain
 # Copyright 2020 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
